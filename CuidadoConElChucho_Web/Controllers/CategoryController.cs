a34c7ad51e0199b5f8a603a6ed99b9550670bdd3
EMSAREES <emiliosanielre@gmail.com>
2026-08-07 16:23:56 -0700

    feat: add functionality in CategoryController with CategoryVM to display categories in their own window

---
+++ CuidadoConElChucho_Web/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CuidadoConElChucho_Web.Context;
+using CuidadoConElChucho_Web.Models;
+
+namespace CuidadoConElChucho_Web.Controllers
+{
+    public class CategoryController(AppDbContext _dbContext) : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = _dbContext.Categories.Select(item =>
+                new CategoryVM
+                {
+                    CategoryId = item.CategoryId,
+                    Name = item.Name
+                }).ToList();
+
+            return View(categories);
+        }
+    }
+}
---
+++ CuidadoConElChucho_Web/Models/CategoryVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CuidadoConElChucho_Web.Models
+{
+    public class CategoryVM
+    {
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
